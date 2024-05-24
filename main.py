25ca95643d26d799671e9968fc084577f4ac792e
Huiwen <huiwennear@163.com>
2024-05-24 17:56:00 +0800

    feat:添加项目2

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author : lihuiwen
+# @file : goods_spider
+# @Email : huiwennear@163.com
+# @Time : 2024/5/23 14:00
+from scrapy.cmdline import execute
+
+execute(['scrapy', 'crawl', 'goods_spider'])
